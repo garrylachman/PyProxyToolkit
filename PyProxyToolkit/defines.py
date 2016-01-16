d731cf1272ddc13fbb4a100853d6f1b36c8c4b09
garrylachman <drgarry@gmail.com>
2016-01-16 05:36:34 +0200

    gnu gpl

--- PyProxyToolkit/defines.py
+++ PyProxyToolkit/defines.py
@@ -1,3 +1,16 @@
+"""
+Copyright (C) 2016 Garry Lachman garry@lachman.co under GNU LGPL
+https://github.com/garrylachman/PyProxyToolkit
+https://rev.proxies.online
+
+This library is free software; you can redistribute it and/or modify it under the terms of the
+GNU Lesser General Public License version 2.1, as published by the Free Software Foundation.
+
+This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Lesser General Public License for more details.
+"""
+
 def defines():
     defines.LOGGER_NAME = 'PyProxyToolkit'
     defines.LOGGER_FILE = 'py_proxy_toolkit.log'
